1f0dfc9319be96d680f779ce4ad685c2d82ebd34
Mattiud <mstias.sanhueza30@gmail.com>
2024-12-02 16:40:27 -0300

    Add files via upload

---
+++ readme.txt
@@ -0,0 +1,5 @@
+CRUD con PHP y MySQL
+
+El programa permite visualizar los usuarios registrados con su respectivo id, nombre, apellido, email, contraseña y nombre de usuario.
+
+El host debe ser local con XAMPP.
---
+++ update.php
@@ -0,0 +1,37 @@
+<?php
+include('connection.php');
+$con = connection();
+
+$id=$_GET['id'];
+
+$sql = "SELECT * FROM users WHERE id='$id'";
+$query = mysqli_query($con,$sql);
+$row = mysqli_fetch_array($query);
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="CSS/style.css">
+    <title>Editar usuarios</title>
+</head>
+
+<body>
+    <div class="users-form">
+        <form action="edit_user.php" method="POST">
+            <h1>Editar Usuario</h1>
+            <input type="hidden" name="id" value="<?= $row['id'] ?>">
+            <input type="text" name="name" placeholder="Nombre" value="<?= $row['name'] ?>">
+            <input type="text" name="lastname" placeholder="Apellido" value="<?= $row['lastname'] ?>">
+            <input type="text" name="username" placeholder="Nombre de usuario" value="<?= $row['username'] ?>">
+            <input type="text" name="password" placeholder="Contraseña" value="<?= $row['password'] ?>">
+            <input type="text" name="email" placeholder="Email" value="<?= $row['email'] ?>">
+
+            <input type="submit" value ="Actualizar usuario">
+        </form>
+    </div>
+</body>
+</html>
